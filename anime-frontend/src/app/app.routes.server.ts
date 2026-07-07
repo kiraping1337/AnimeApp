efa17e2e6d6dc21b5843eec18d0126fcf8015c09
kiraping <belaev393@gmail.com>
2026-07-07 10:43:12 +0400

    fix(config): increase styles budget and disable prerender

--- anime-frontend/src/app/app.routes.server.ts
+++ anime-frontend/src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'tierlists/:id/edit',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'favorites',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
